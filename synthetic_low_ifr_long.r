f271a639573a23ddc398f3e97dbfc04aafc155a6
Gustav Delius <gustav.delius@gmail.com>
2020-05-02 17:58:46 +0100

    run for longer and with 8 chains

---
+++ synthetic_low_ifr_long.r
@@ -0,0 +1,38 @@
+library(rstan)
+load("results/base-stanfit.Rdata")
+
+# data
+M <- stan_data$M  # number of countries
+N <- stan_data$N  # days of observed data for country m. each entry must be <= N2
+EpidemicStart <- stan_data$EpidemicStart
+
+# parameters
+# We choose those from the posterior sample with the highest density
+out <- rstan::extract(fit)
+idx <- which.max(out$lp__)
+phi <- out$phi[idx]
+E_deaths <- out$E_deaths[idx, , ]
+
+# model
+for(m in 1:M){
+  for (i in EpidemicStart[m]:N[m]) {
+    stan_data$deaths[i, m] <- rnbinom(1, mu = E_deaths[i, m], size = phi)
+    }
+}
+
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+StanModel <- "flat_low_ifr"
+m = stan_model(paste0('stan-models/',StanModel,'.stan'))
+fit = sampling(
+  m,
+  data = stan_data,
+  iter = 2800,
+  warmup = 2000,
+  chains = 8,
+  thin = 1,
+  control = list(adapt_delta = 0.95, max_treedepth = 15)
+)
+
+save(fit, dates, reported_cases, deaths_by_country, countries, stan_data, 
+     file = paste0('results/',StanModel,'-synthetic.Rdata'))
